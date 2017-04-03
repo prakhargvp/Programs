389747430608e916d5c51d3c52dff3ecc566272e
prakhargvp <prakhargvp@gmail.com>
2017-04-03 16:12:34 +0530

    Adding Insertion Sort in Shell-Script using while-loop and expr 	modified:   1.sh

---
+++ Sorting/insertion_sort/algo-1/1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Read Size of Array
+read N
+i=0
+# Read elements of Array
+while [ "$i" -lt "$N" ]
+do
+	read A[$i]
+	# maintain space between operator
+	# expr is treate $i as number
+	i=`expr $i + 1`
+done
+# Insertion Sort Algorithm
+i=1
+while [ "$i" -lt "$N" ]
+do
+	key=${A[$i]}
+	j=`expr $i - 1`
+	while [ "$j" -ge 0 ] && [ "${A[$j]}" -gt "$key" ]
+	do
+		A[`expr $j + 1`]=${A[$j]}
+		j=`expr $j - 1`
+	done
+	A[`expr $j + 1`]=$key 
+	i=`expr $i + 1`
+done
+
+# Output
+i=0
+while [ "$i" -lt "$N" ]
+do
+	echo -n ${A[$i]}" "
+	i=`expr $i + 1`
+done
--- Sorting/insertion_sort/algo-1/readme.txt
+++ Sorting/insertion_sort/algo-1/readme.txt
@@ -6,6 +6,7 @@
 1.cpp   : Implementing Insertion sort in C++ using new keyword
 1-1.cpp : Implementing Insertion sort in C++ using vector
 1.php   : Implementing Insertion sort in php
+1.sh	: Implementing Insertion SOrt in Shell-Script using while-loop and expr
 
 [Functions]
 [Syntax]
